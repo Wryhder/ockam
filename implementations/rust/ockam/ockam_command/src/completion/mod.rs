3dc3fcfa5c585b4ed12e15024266f484cfb379ee
Wryhder <jemimahsummers@yahoo.com>
2024-11-18 19:06:21 +0100

    feat(rust): add "unsafe" tag to commands that are considered unsafe
    Signed-off-by: Wryhder <jemimahsummers@yahoo.com>

--- implementations/rust/ockam/ockam_command/src/completion/mod.rs
+++ implementations/rust/ockam/ockam_command/src/completion/mod.rs
@@ -7,6 +7,7 @@ use crate::{docs, OckamCommand};
 
 const LONG_ABOUT: &str = include_str!("./static/long_about.txt");
 const PREVIEW_TAG: &str = include_str!("../static/preview_tag.txt");
+const UNSAFE_TAG: &str = include_str!("../static/unsafe_tag.txt");
 const AFTER_LONG_HELP: &str = include_str!("./static/after_long_help.txt");
 
 /// Generate shell completion scripts
@@ -15,6 +16,8 @@ const AFTER_LONG_HELP: &str = include_str!("./static/after_long_help.txt");
     arg_required_else_help = true,
     long_about = docs::about(LONG_ABOUT),
     before_help = docs::before_help(PREVIEW_TAG),
+    // TODO: What's the correct way to include the unsafe tag here?
+    before_help = docs::before_help(UNSAFE_TAG),
     after_long_help = docs::after_help(AFTER_LONG_HELP)
 )]
 pub struct CompletionCommand {
